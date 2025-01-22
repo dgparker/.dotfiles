72dead70901cf7c534bd707baf4961861093e862
dgparker <dylan@codecave.appw>
2025-01-22 09:03:58 -0600

    aerospace workspace automation, nvim theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -842,22 +842,20 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
-    lazy = false,
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      require('github-theme').setup {
-        options = {
-          transparent = true,
-        },
-      }
-    end,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'github_dark_colorblind'
+      vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
